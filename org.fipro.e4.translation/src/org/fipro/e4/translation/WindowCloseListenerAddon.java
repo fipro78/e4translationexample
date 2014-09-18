afd2a2a1295611e1ec4bcd6e93b19f7b9836234e
Dirk Fauth <dirk.fauth@googlemail.com>
2014-09-18 10:33:41 +0200

    Added handling for adding a new window at runtime

--- org.fipro.e4.translation/src/org/fipro/e4/translation/WindowCloseListenerAddon.java
+++ org.fipro.e4.translation/src/org/fipro/e4/translation/WindowCloseListenerAddon.java
@@ -3,43 +3,84 @@ package org.fipro.e4.translation;
 import javax.inject.Inject;
 
 import org.eclipse.e4.core.di.annotations.Optional;
-import org.eclipse.e4.core.services.events.IEventBroker;
+import org.eclipse.e4.ui.di.UIEventTopic;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.e4.ui.workbench.IWorkbench;
 import org.eclipse.e4.ui.workbench.UIEvents;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
 import org.eclipse.e4.ui.workbench.modeling.IWindowCloseHandler;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
 import org.osgi.service.event.Event;
-import org.osgi.service.event.EventHandler;
 
-/**
- * Addon which registers an IWindowCloseHandler that closes all windows of the application.
- */
+// @PostConstruct will not work as workbench gets instantiated after the processing of the add-ons
+// hence this approach uses method injection
+
 public class WindowCloseListenerAddon {
 
 	@Inject
+	MApplication application;
+	
+	@Inject
+	EPartService partService;
+	
+	// as the IWorkbench is not available at creation time, we need to annotate it with
+	// @Optional so it gets reinjected once it is created
+	
+	@Inject
 	@Optional
-	void registerWindowCloseListener(
-			final IEventBroker eventBroker, final MApplication application, final IWorkbench workbench) {
-		final IWindowCloseHandler closeHandler = new IWindowCloseHandler() {
-
-			@Override
-			public boolean close(MWindow window) {
-				if (workbench != null) {
-					workbench.close();
-				}
-				return true;
+	IWorkbench workbench;
+	
+	// while initializing a class, all annotated methods are sequentially called
+	// therefore we need to annotate the following methods with @Optional as the
+	// events are not present at the creation time of this add-on instance
+	
+	@Inject
+	@Optional
+	private void subscribeApplicationCompleted(@UIEventTopic(UIEvents.UILifeCycle.APP_STARTUP_COMPLETE) Event event) {
+		if (workbench != null ){
+			registerCloseHandler(application);
+		}
+	}
+	
+	@Inject
+	@Optional
+	private void subscribeTopicChildrenChanged(@UIEventTopic(UIEvents.ElementContainer.TOPIC_CHILDREN) Event event) {
+		Object changedObj = event.getProperty(UIEvents.EventTags.ELEMENT);
+		
+		// only interested in changes to application
+		if (changedObj instanceof MApplication) {
+			MApplication application = (MApplication) changedObj;
+			if (workbench != null ) {
+				registerCloseHandler(application);
 			}
-		};
+		}
+	}
 
-		eventBroker.subscribe(UIEvents.UILifeCycle.APP_STARTUP_COMPLETE, new EventHandler() {
+	private void registerCloseHandler(MApplication application) {
+		
+		// each window gets its own close handler as we want to add a modal confirm dialog
+		for (MWindow window : application.getChildren()) {
+
+			final Shell shell = (Shell) window.getWidget();
 			
-			@Override
-			public void handleEvent(Event event) {
-				for (MWindow window : application.getChildren()) {
-					window.getContext().set(IWindowCloseHandler.class, closeHandler);
+			IWindowCloseHandler closeHandler = new IWindowCloseHandler() {
+
+				@Override
+				public boolean close(MWindow window) {
+					boolean close = true;
+					if (partService.getDirtyParts().size() > 0) {
+						close = MessageDialog.openConfirm(shell, "Unsaved data", "Really close?");
+					}
+					if (close) {
+						workbench.close();
+					}
+					return close;
 				}
-			}
-		});
+			};
+			
+			window.getContext().set(IWindowCloseHandler.class, closeHandler);
+		}
 	}
 }
---
+++ org.fipro.e4.translation/src/org/fipro/e4/translation/control/WindowAddHandler.java
@@ -0,0 +1,29 @@
+ 
+package org.fipro.e4.translation.control;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
+
+public class WindowAddHandler {
+	
+	@Execute
+	public void execute(EModelService modelService, MApplication application) {
+		// create a new window and set its size
+		MWindow window = modelService.createModelElement(MWindow.class);
+		window.setX(1025);
+		window.setY(550);
+		window.setWidth(400);
+		window.setHeight(200);
+		
+		// need the contributor URI so the translation service is able to retrieve 
+		// the translations
+		window.setContributorURI("platform:/plugin/org.fipro.e4.translation");
+		window.setLabel("%application");
+
+		// add new Window to the application
+		application.getChildren().add(window); 	
+	}
+		
+}
